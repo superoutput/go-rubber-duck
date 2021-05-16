a465450854d4db2a0fd8470b0b6093f063fb1f36
Savage Lasa <superoutput@gmail.com>
2021-05-16 21:46:08 +0700

    Complete the first version of duck avatar character.

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/superoutput/go-rubber-duck
+
+go 1.16
+
+require (
+	github.com/JoelOtter/termloop v0.0.0-20201118115657-7fa23b4da654 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,54 +1,97 @@
 package main
 
 import "fmt"
-// import "os"
-// import "github.com/urfave/cli/v2"
+import "math/rand"
+import "os"
+import "time"
+import "github.com/urfave/cli/v2"
 import tl "github.com/JoelOtter/termloop"
 
 type Avatar struct {
 	*tl.Entity
 }
 
-// create canvas with duck avatar
-func createCanvas(color tl.Attr) *tl.Canvas {
+type Mouth struct {
+	*tl.Entity
+}
+
+type Eye struct {
+	*tl.Entity
+}
+
+// create duck avatar body
+func createBody(color tl.Attr) *tl.Canvas {
 	canvas := tl.NewCanvas(500, 500)
-	draw(canvas, 19, 10, "▀▄▄▂")
-	draw(canvas, 26, 10, "▂▄█▀")
-	draw(canvas, 21, 11, "▀█▄")
-	draw(canvas, 25, 11, "▄██▀")
-	draw(canvas, 16, 12, "▂▄▄▄██████████▄▄▄▂")
-	draw(canvas, 13, 13, "▂▄████████████████████▄▂")
-	draw(canvas, 12, 14, "▄████████████████████████▄")
-	draw(canvas, 11, 15, "▄██████████████████████████▄")
-	draw(canvas, 11, 16, "▀██████████████████████████▀")
-	draw(canvas, 11, 17, "▄██████████████████████████▄")
-	draw(canvas, 9, 18, "▂▄████████████████████████████▄▂")
-	draw(canvas, 8, 19, "▄████████████████████████████████▄")
-	draw(canvas, 8, 20, "▀████████████████████████████████▀")
-	draw(canvas, 10, 21, "▀▀▀▀██████████████████████▀▀▀▀")
-	draw(canvas, 16, 22, "▀▀▀▀██████████▀▀▀▀")
-	draw(canvas, 16, 23, "▂▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▂")
-	draw(canvas, 13, 24, "▄██████████████████████▄")
-	draw(canvas, 11, 25, "▄██████████████████████████▄")
-	draw(canvas, 11, 26, "▄██████████████████████████▄")
-	draw(canvas, 11, 27, "▀██████████████████████████▀")
-	draw(canvas, 12, 28, "▀████████████████████████▀")
-	draw(canvas, 13, 29, "▀▀████████████████████▀▀")
-	draw(canvas, 16, 30, "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
-	
+	cell := tl.Cell{
+		Bg: tl.ColorDefault,
+		Fg: tl.RgbTo256Color(252, 226, 5),
+	}
+
+	draw(canvas, cell, 19, 10, "▀▄▄▂")
+	draw(canvas, cell, 26, 10, "▂▄█▀")
+	draw(canvas, cell, 21, 11, "▀█▄")
+	draw(canvas, cell, 25, 11, "▄██▀")
+	draw(canvas, cell, 16, 12, "▂▄▄▄██████████▄▄▄▂")
+	draw(canvas, cell, 13, 13, "▂▄████████████████████▄▂")
+	draw(canvas, cell, 12, 14, "▄████████████████████████▄")
+	draw(canvas, cell, 12, 15, "██████████████████████████")
+	draw(canvas, cell, 12, 16, "██████████████████████████")
+	draw(canvas, cell, 11, 17, "▄██████████████████████████▄")
+	draw(canvas, cell, 9, 18, "▄██████████████████████████████▄")
+	draw(canvas, cell, 8, 19, "▄████████████████████████████████▄")
+	draw(canvas, cell, 8, 20, "▀████████████████████████████████▀")
+	draw(canvas, cell, 10, 21, "▀▀▀▀██████████████████████▀▀▀▀")
+	draw(canvas, cell, 16, 22, "▀▀▀▀██████████▀▀▀▀")
+	draw(canvas, cell, 16, 23, "▂▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▂")
+	draw(canvas, cell, 13, 24, "▄██████████████████████▄")
+	draw(canvas, cell, 11, 25, "▄██████████████████████████▄")
+	draw(canvas, cell, 11, 26, "▄██████████████████████████▄")
+	draw(canvas, cell, 11, 27, "▀██████████████████████████▀")
+	draw(canvas, cell, 12, 28, "▀████████████████████████▀")
+	draw(canvas, cell, 13, 29, "▀▀████████████████████▀▀")
+	draw(canvas, cell, 16, 30, "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀")
+
 	return &canvas
 }
 
-func draw(canvas tl.Canvas, x, y int, s string) {
+// create duck avatar's mouth
+func createMouth(color tl.Attr) *tl.Canvas {
+	canvas := tl.NewCanvas(500, 500)
+	cell := tl.Cell{
+		// Bg: tl.RgbTo256Color(255, 250, 80),
+		Bg: tl.RgbTo256Color(255, 210, 15),
+		Fg: tl.RgbTo256Color(255, 131, 0),
+	}
+
+	draw(canvas, cell, 20, 18, "▂▄▄████▄▄▂")
+	draw(canvas, cell, 13, 19, "▂▄▂▂▄██████████████▄▂▂▄▂")
+	draw(canvas, cell, 13, 20, "▀█▀▀▀███▄▄▂▂▂▂▄▄███▀▀▀█▀")
+	draw(canvas, cell, 19, 21, "▀▀▀██████▀▀▀")
+
+	return &canvas
+}
+
+// create duck avatar's eyes
+func createEyes(color tl.Attr) *tl.Canvas {
+	canvas := tl.NewCanvas(500, 500)
+	cell := tl.Cell{
+		Bg: tl.RgbTo256Color(255, 210, 15),
+		Fg: tl.ColorBlack,
+	}
+
+	draw(canvas, cell, 17, 15, "▂▄▀▀▀▄▂")
+	draw(canvas, cell, 27, 15, "▂▄▀▀▀▄▂")
+	draw(canvas, cell, 16, 16, "▄▀▄██▄ █")
+	draw(canvas, cell, 27, 16, "█ ▄██▄▀▄")
+	draw(canvas, cell, 16, 17, "█▄███▂▄▀")
+	draw(canvas, cell, 27, 17, "▀▄▂███▄█")
+
+	return &canvas
+}
+
+func draw(canvas tl.Canvas, cell tl.Cell, x, y int, s string) {
 	for i, c := range []rune(s) {
-		cell := tl.Cell{
-			// Bg: tl.ColorBlack,
-			Bg: tl.RgbTo256Color(255, 195, 11),
-			// Fg: tl.ColorYellow,
-			Fg: tl.RgbTo256Color(252, 226, 5),
-			Ch: c,
-		}
-		canvas[x+i][y].Ch = cell.Ch
+		canvas[x+i][y].Ch = c
 		canvas[x+i][y].Bg = cell.Bg
 		canvas[x+i][y].Fg = cell.Fg
 	}
@@ -56,20 +99,22 @@ func draw(canvas tl.Canvas, x, y int, s string) {
 
 func main() {
 	fmt.Println("Hello, Little Duck")
-	// (&cli.App{}).Run(os.Args)
+	rand.Seed(time.Now().UTC().UnixNano())
+	(&cli.App{}).Run(os.Args)
+
 	game := tl.NewGame()
 	level := tl.NewBaseLevel(tl.Cell{
 		Bg: tl.ColorDefault,
 		Fg: tl.ColorBlack,
 		Ch: '.',
 	})
-	// level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
-	// player := Player{tl.NewEntity(1, 1, 1, 1)}
-	// Set the character at position (0, 0) on the entity.
-	// player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
-	avatar := Avatar{tl.NewEntityFromCanvas(0, 0, *createCanvas(tl.ColorRed))}
-	// game.Screen().AddEntity(&avatar)
+
+	avatar := Avatar{tl.NewEntityFromCanvas(0, 0, *createBody(tl.ColorRed))}
+	mouth := Mouth{tl.NewEntityFromCanvas(0, 0, *createMouth(tl.ColorRed))}
+	eye := Eye{tl.NewEntityFromCanvas(0, 0, *createEyes(tl.ColorRed))}
 	level.AddEntity(&avatar)
+	level.AddEntity(&mouth)
+	level.AddEntity(&eye)
 	game.Screen().SetLevel(level)
 	game.Start()
 }
